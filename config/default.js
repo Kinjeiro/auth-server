8f38b78f36409c5fa6ea5bc3fcb9f01643c57ea7
Andrey Kuzmin <kinjeiro@gmail.com>
2019-03-24 13:23:59 +0300

    feat(config, ssl): - добавил CUSTOM_CONFIG (для переопределения конфиги при старте сервера) \\ добавил chain сертификат для ssl

--- config/default.js
+++ config/default.js
@@ -1,4 +1,7 @@
 const path = require('path');
+const {
+  extendDeep,
+} = require('config/lib/util');
 
 const defaultLogsPath = `${process.cwd()}/logs/all.log`;
 
@@ -17,189 +20,207 @@ const {
   DROP_ON_START = false,
   CONTEXT_PATH,
   NO_SSL_CERTIFICATES = false,
+  CUSTOM_CONFIG,
 } = process.env;
 
 const env = NODE_ENV;
 
-module.exports = {
-  common: {
-    env,
-    isProduction: env === 'production',
-    isTest: env === 'test',
-    appId: APP_ID,
-    appVersion: APP_VERSION,
+const customConfig = CUSTOM_CONFIG ? JSON.parse(CUSTOM_CONFIG) : undefined;
 
-    app: {
-      contextPath: CONTEXT_PATH,
-    },
-  },
+module.exports = extendDeep(
+  {
+    common: {
+      env,
+      isProduction: env === 'production',
+      isTest: env === 'test',
+      appId: APP_ID,
+      appVersion: APP_VERSION,
 
-  server: {
-    /*
-      @NOTE: ВАЖНО! тут прописываются пути до родительских конфигов, к примеру parentConfigs: [''],
-    */
-    parentConfigs: null,
-
-    main: {
-      port: PORT,
-      // актуально для размера аватарки пользователя
-      maxContentSize: '3mb',
-
-      expressServerOptions: {},
+      app: {
+        contextPath: CONTEXT_PATH,
+      },
     },
 
-    features: {
-      sslCertificates: NO_SSL_CERTIFICATES
-        ? false
-        : {
-          /*
-            если на сервере используется SSL сертификат через nginx - отключите это, просто проставив null
-          */
-          privateKey: path.resolve('./demo-cert/demo.local.key'),
-          certificate: path.resolve('./demo-cert/demo.local.crt'),
+    server: {
+      /*
+        @NOTE: ВАЖНО! тут прописываются пути до родительских конфигов, к примеру parentConfigs: [''],
+      */
+      parentConfigs: null,
+
+      main: {
+        port: PORT,
+        // актуально для размера аватарки пользователя
+        maxContentSize: '3mb',
+
+        expressServerOptions: {},
+      },
+
+      features: {
+        sslCertificates: NO_SSL_CERTIFICATES
+          ? false
+          : {
+            /*
+              если на сервере используется SSL сертификат через nginx - отключите это, просто проставив null
+
+
+              const privateKey = fs.readFileSync('/etc/letsencrypt/live/yourdomain.com/privkey.pem', 'utf8');
+              const certificate = fs.readFileSync('/etc/letsencrypt/live/yourdomain.com/cert.pem', 'utf8');
+              const ca = fs.readFileSync('/etc/letsencrypt/live/yourdomain.com/chain.pem', 'utf8');
+
+              const credentials = {
+                key: privateKey,
+                cert: certificate,
+                ca: ca
+              };
+            */
+            privateKey: path.resolve('./demo-cert/demo.local.key'),
+            certificate: path.resolve('./demo-cert/demo.local.crt'),
+            chain: undefined,
+          },
+
+        security: {
+          callbackAccessTokenParam: 'accessToken',
+          callbackAccessTokenLifeParam: 'accessTokenLife',
+          callbackRefreshTokenParam: 'refreshToken',
+          callbackRefreshTokenLifeParam: 'refreshTokenLife',
+
+          token: {
+            // секунд
+            tokenLife: 3600, // час
+            refreshTokenLife: 604800, // неделя
+          },
         },
 
-      security: {
-        callbackAccessTokenParam: 'accessToken',
-        callbackAccessTokenLifeParam: 'accessTokenLife',
-        callbackRefreshTokenParam: 'refreshToken',
-        callbackRefreshTokenLifeParam: 'refreshTokenLife',
+        logger: {
+          winston: {
+            exitOnError: false,
+          },
 
-        token: {
-          // секунд
+          transports: {
+            fileLogger: {
+              id: 'fileLogger',
+              type: 'file',
+              level: 'info',
+              filename: LOGS_PATH,
+              handleException: true,
+              json: true,
+              maxSize: 5242880, // 5mb
+              maxFiles: 2,
+              colorize: false,
+            },
+            consoleLogger: {
+              id: 'consoleLogger',
+              type: 'console',
+              level: 'debug',
+              // label: getFilePath(module),
+              handleException: true,
+              json: false,
+              colorize: true,
+            },
+          },
+        },
+
+        db: {
+          mongoose: {
+            uri: MONGO_URI || 'mongodb://localhost:27017/auth-server',
+            testUri: TEST_MONGO_URI || 'mongodb://localhost:27017/auth-serverTest',
+            auth: {
+              user: MONGO_USER || 'authServerUser',
+              password: MONGO_PASSWORD || 'authAuth',
+            },
+          },
+          dropOnStart: DROP_ON_START,
+        },
+
+        mail: {
+          transportOptions: {
+            // service: 'SendGrid',
+            // FREE: 100 mails in month
+            host: 'smtp.sendgrid.net',
+            /*
+             Ports
+             25, 587  - (for unencrypted/TLS connections)
+             465      - (for SSL connections)
+             */
+            port: 587,
+
+            // security: false,
+
+            /*
+             Если не проставлять падает ошибка:
+             code=ESOCKET, command=CONN
+             Error: self signed certificate in certificate chain
+             at TLSSocket.onConnectSecure (_tls_wrap.js:1036:3
+             */
+            tls: {
+              rejectUnauthorized: false,
+            },
+
+            // connectionTimeout: 60000,
+            // greetingTimeout: 30000,
+            // // maxConnections: 10,
+            // debug: true,
+
+            auth: {
+              user: 'apikey',
+              // Andrey (kinjeiro@gmail.com) account key
+              pass: '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',
+            },
+          },
+          messageOptions: {
+            from: 'kinjeiro@gmail.com',
+          },
+        },
+
+        // Andrey (kinjeiro@gmail.com) account key
+        defaultProviderCredentials: {
+          google: {
+            /*
+              https://codd-wd.ru/shpargalka-poluchenie-tokenov-dlya-raboty-s-api-servisov-google/
+
+              https://console.developers.google.com/apis/credentials/oauthclient/756888674250-kd5a9k8b007q3t65n9s9m03bmdasrp0n.apps.googleusercontent.com?project=reagentum-231315&hl=ru
+              там нужно добавить точные редиректы:
+                https://dev.reagentum.ru:1338/api/auth/google/callback
+                https://front.reagentum.ru:1338/api/auth/google/callback
+                https://127.0.0.1:1338/api/auth/google/callback
+
+              зачем затребовал включить Google Plus Api
+            */
+            clientID: '756888674250-kd5a9k8b007q3t65n9s9m03bmdasrp0n.apps.googleusercontent.com',
+            clientSecret: '5gAB3bbipBX0HPuxJroMZvjT',
+          },
+          facebook: {
+            /*
+              https://developers.facebook.com/apps/2226184230986091/fb-login/settings/
+              - обязательно https
+              - Действительные URI перенаправления для OAuth
+                Нужен точный до апи метод указывать в редиректе:
+                https://127.0.0.1:1338/api/auth/facebook/callback
+
+              https://developers.facebook.com/docs/facebook-login/access-tokens/refreshing
+                - долгосрочный маркер с помощью code
+            */
+            clientID: 2226184230986091,
+            clientSecret: '6b95d53ebaa413b851c21411cdfb3f6f',
+          },
+          vkontakte: {
+            // https://codd-wd.ru/primery-realizacii-avtorizacii-oauth-2-0-vkontakte-na-php-i-javascript-vk-api/
+            // https://vk.com/editapp?id=6855085&section=options
+            /*
+              если тестируем не забудь добавить в базовый домен 127.0.0.1 а "доверенный урл" должен быть пустым
+            */
+            clientID: '6855085',
+            clientSecret: 'k3x4cn9PWb3dwGuBKefz',
+          },
+          // todo @ANKU @LOW - social auth - yandex
+          // https://codd-wd.ru/primery-avtorizaciya-poluchenie-access-token-oauth-2-0-yandeks-i-rabota-s-api-na-php/
+        },
+
+        resetPassword: {
           tokenLife: 3600, // час
-          refreshTokenLife: 604800, // неделя
         },
-      },
-
-      logger: {
-        winston: {
-          exitOnError: false,
-        },
-
-        transports: {
-          fileLogger: {
-            id: 'fileLogger',
-            type: 'file',
-            level: 'info',
-            filename: LOGS_PATH,
-            handleException: true,
-            json: true,
-            maxSize: 5242880, // 5mb
-            maxFiles: 2,
-            colorize: false,
-          },
-          consoleLogger: {
-            id: 'consoleLogger',
-            type: 'console',
-            level: 'debug',
-            // label: getFilePath(module),
-            handleException: true,
-            json: false,
-            colorize: true,
-          },
-        },
-      },
-
-      db: {
-        mongoose: {
-          uri: MONGO_URI || 'mongodb://localhost:27017/auth-server',
-          testUri: TEST_MONGO_URI || 'mongodb://localhost:27017/auth-serverTest',
-          auth: {
-            user: MONGO_USER || 'authServerUser',
-            password: MONGO_PASSWORD || 'authAuth',
-          },
-        },
-        dropOnStart: DROP_ON_START,
-      },
-
-      mail: {
-        transportOptions: {
-          // service: 'SendGrid',
-          // FREE: 100 mails in month
-          host: 'smtp.sendgrid.net',
-          /*
-           Ports
-           25, 587  - (for unencrypted/TLS connections)
-           465      - (for SSL connections)
-           */
-          port: 587,
-
-          // security: false,
-
-          /*
-           Если не проставлять падает ошибка:
-           code=ESOCKET, command=CONN
-           Error: self signed certificate in certificate chain
-           at TLSSocket.onConnectSecure (_tls_wrap.js:1036:3
-           */
-          tls: {
-            rejectUnauthorized: false,
-          },
-
-          // connectionTimeout: 60000,
-          // greetingTimeout: 30000,
-          // // maxConnections: 10,
-          // debug: true,
-
-          auth: {
-            user: 'apikey',
-            // Andrey (kinjeiro@gmail.com) account key
-            pass: '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',
-          },
-        },
-        messageOptions: {
-          from: 'kinjeiro@gmail.com',
-        },
-      },
-
-      // Andrey (kinjeiro@gmail.com) account key
-      defaultProviderCredentials: {
-        google: {
-          /*
-            https://codd-wd.ru/shpargalka-poluchenie-tokenov-dlya-raboty-s-api-servisov-google/
-
-            https://console.developers.google.com/apis/credentials/oauthclient/756888674250-kd5a9k8b007q3t65n9s9m03bmdasrp0n.apps.googleusercontent.com?project=reagentum-231315&hl=ru
-            там нужно добавить точные редиректы:
-              https://dev.reagentum.ru:1338/api/auth/google/callback
-              https://front.reagentum.ru:1338/api/auth/google/callback
-              https://127.0.0.1:1338/api/auth/google/callback
-
-            зачем затребовал включить Google Plus Api
-          */
-          clientID: '756888674250-kd5a9k8b007q3t65n9s9m03bmdasrp0n.apps.googleusercontent.com',
-          clientSecret: '5gAB3bbipBX0HPuxJroMZvjT',
-        },
-        facebook: {
-          /*
-            https://developers.facebook.com/apps/2226184230986091/fb-login/settings/
-            - обязательно https
-            - Действительные URI перенаправления для OAuth
-              Нужен точный до апи метод указывать в редиректе:
-              https://127.0.0.1:1338/api/auth/facebook/callback
-
-            https://developers.facebook.com/docs/facebook-login/access-tokens/refreshing
-              - долгосрочный маркер с помощью code
-          */
-          clientID: 2226184230986091,
-          clientSecret: '6b95d53ebaa413b851c21411cdfb3f6f',
-        },
-        vkontakte: {
-          // https://codd-wd.ru/primery-realizacii-avtorizacii-oauth-2-0-vkontakte-na-php-i-javascript-vk-api/
-          // https://vk.com/editapp?id=6855085&section=options
-          /*
-            если тестируем не забудь добавить в базовый домен 127.0.0.1 а "доверенный урл" должен быть пустым
-          */
-          clientID: '6855085',
-          clientSecret: 'k3x4cn9PWb3dwGuBKefz',
-        },
-        // todo @ANKU @LOW - social auth - yandex
-        // https://codd-wd.ru/primery-avtorizaciya-poluchenie-access-token-oauth-2-0-yandeks-i-rabota-s-api-na-php/
-      },
-
-      resetPassword: {
-        tokenLife: 3600, // час
       },
     },
   },
-};
+  customConfig
+);
--- src/ServerRunner.js
+++ src/ServerRunner.js
@@ -99,13 +99,11 @@ export default class ServerRunner {
   }
 
   initServer() {
-    if (!config.common.isProduction) {
-      logger.debug(
-        '=== SERVER CONFIG ===\n',
-        JSON.stringify(config, null, 2),
-        '\n\n',
-      );
-    }
+    logger.debug(
+      '=== SERVER CONFIG ===\n',
+      JSON.stringify(config, null, 2),
+      '\n\n',
+    );
 
     this.app = this.createApp(config.server.main.expressServerOptions);
 
@@ -148,10 +146,14 @@ export default class ServerRunner {
       // todo @ANKU @LOW - сделать для chai request rejectUnauthorized false - https://github.com/chaijs/chai-http/issues/180
       const privateKey  = fs.readFileSync(config.server.features.sslCertificates.privateKey, 'utf8');
       const certificate = fs.readFileSync(config.server.features.sslCertificates.certificate, 'utf8');
+      const chain = config.server.features.sslCertificates.chain && fs.readFileSync(config.server.features.sslCertificates.chain, 'utf8');
+
       const credentials = {
         key: privateKey,
         cert: certificate,
+        ca: chain,
       };
+
       server = https.createServer(credentials, app);
     }
 
